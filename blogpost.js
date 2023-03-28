c6eba1896de323c9173fdd885158f6fae8388260
MichaelOsterhus <michaelosterhus278@gmail.com>
2023-03-28 12:59:52 -0400

    Getting Blog To Link

--- blogpost.js
+++ blogpost.js
@@ -1,6 +1,7 @@
 
 var apiKey = 'AIzaSyBgbqYoW1taQx5bY59XETPF48N1SzyWxSE'
-var blogId = 'trannyinacan'
+var blogId = '4366616470796725508'
+var Thrimskvida = '6208382948984439349'
 
 function getBloggerData(postId) {
   var apiUrl = 'https://www.googleapis.com/blogger/v3/blogs/' + blogId + '/posts/' + postId + '?key=' + apiKey + '&fields=title,selfLink,images&fetchImages=true';
@@ -8,7 +9,8 @@ function getBloggerData(postId) {
   $.getJSON(apiUrl, function(data) {
     $('#post-title').text(data.title);
     $('#post-image').attr('src', data.images[0].url);
-  });.fail(function(jqXHR, textStatus, errorThrown) {
+    $('#post-link').attr('href', data.selfLink);
+  }).fail(function(jqXHR, textStatus, errorThrown) {
     console.log(jqXHR);
     console.log(textStatus);
     console.log(errorThrown);
@@ -16,5 +18,5 @@ function getBloggerData(postId) {
 }
 
 
-getBloggerData('a-sign-on-my-journey-to-gender-deviance');
+getBloggerData(Thrimskvida);
 
