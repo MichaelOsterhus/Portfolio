887e568454a93069f19366a3dac44e8c45bca7b7
MichaelOsterhus <michaelosterhus278@gmail.com>
2023-03-31 12:12:12 -0400

    Refining Code with header.js and menu.js

--- js/header.js
+++ js/header.js
@@ -1,44 +1,12 @@
+const headContent = `<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="apple-touch-icon" sizes="180x180" href="img/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="img/favicon-16x16.png">
+<link rel="manifest" href="img/site.webmanifest">`
 
-// Create link elements for the CSS files
-var css1 = document.createElement('link');
-css1.rel = 'stylesheet';
-css1.href = 'css/homestyle.css';
+const headElement = document.querySelector('head');
+headElement.insertAdjacentHTML('beforeend', headContent);
 
-var css2 = document.createElement('link');
-css2.rel = 'stylesheet';
-css2.href = 'css/bootstrap.min.css';
-
-// Create link elements for the icon files
-var icon1 = document.createElement('link');
-icon1.rel = 'apple-touch-icon';
-icon1.sizes = '180x180';
-icon1.href = 'img/apple-touch-icon.png';
-
-var icon2 = document.createElement('link');
-icon2.rel = 'icon';
-icon2.type = 'image/png';
-icon2.sizes = '32x32';
-icon2.href = 'img/favicon-32x32.png';
-
-var icon3 = document.createElement('link');
-icon3.rel = 'icon';
-icon3.type = 'image/png';
-icon3.sizes = '16x16';
-icon3.href = 'img/favicon-16x16.png';
-
-var manifest = document.createElement('link');
-manifest.rel = 'manifest';
-manifest.href = 'img/site.webmanifest';
-
-// Create a script element for the Bootstrap JS file
-var script = document.createElement('script');
-script.src = 'js/bootstrap.min.js';
-
-// Append all elements to the head section of the document
-document.head.appendChild(css1);
-document.head.appendChild(css2);
-document.head.appendChild(icon1);
-document.head.appendChild(icon2);
-document.head.appendChild(icon3);
-document.head.appendChild(manifest);
-document.head.appendChild(script);
+console.log('your code works.')
--- js/menu.js
+++ js/menu.js
@@ -1,46 +1,22 @@
-// Create the logo element
-var logoLink = document.createElement('a');
-logoLink.href = 'index.html';
-var logoDiv = document.createElement('div');
-logoDiv.className = 'logo';
-var logoImg = document.createElement('img');
-logoImg.src = 'img/4EC_logo32.png';
-var logoH1 = document.createElement('h1');
-logoH1.textContent = 'Michael Osterhus';
-logoDiv.appendChild(logoImg);
-logoDiv.appendChild(logoH1);
-logoLink.appendChild(logoDiv);
+const menuContent = `<a href="index.html">
+<div class="logo">
+   <img src="img/4EC_logo32.png"/>
+<h1>Michael Osterhus</h1>
+</div>
+</a>
 
-// Create the wrapper element and its child elements
-var wrapperDiv = document.createElement('div');
-wrapperDiv.id = 'wrapper';
-var wrapDiv = document.createElement('div');
-wrapDiv.className = 'wrap';
-var projectsH3 = document.createElement('h3');
-projectsH3.textContent = 'PROJECTS';
-var webdevA = document.createElement('a');
-webdevA.className = 'goal';
-webdevA.href = 'webdev.html';
-webdevA.textContent = 'Web Development';
-var blogsA = document.createElement('a');
-blogsA.className = 'goal';
-blogsA.href = 'blogs.html';
-blogsA.textContent = 'Blogs';
-var vidanimA = document.createElement('a');
-vidanimA.className = 'goal';
-vidanimA.href = 'vidanim.html';
-vidanimA.textContent = 'Video / Animation';
-var digpaintA = document.createElement('a');
-digpaintA.className = 'goal';
-digpaintA.href = 'digpaint.html';
-digpaintA.textContent = 'Digital Painting';
+<div id="wrapper"> 
+<div class="wrap">
 
-// Append all elements to the document body
-wrapDiv.appendChild(projectsH3);
-wrapDiv.appendChild(webdevA);
-wrapDiv.appendChild(blogsA);
-wrapDiv.appendChild(vidanimA);
-wrapDiv.appendChild(digpaintA);
-wrapperDiv.appendChild(wrapDiv);
-document.body.appendChild(logoLink);
-document.body.appendChild(wrapperDiv);
+   <h3>PROJECTS</h3>
+   <a class="goal" href="webdev.html">Web Development</a>
+   <a class="goal" href="blogs.html">Blogs</a>
+   <a class="goal" href="vidanim.html">Video / Animation</a>
+   <a class="goal" href="digpaint.html">Digital Painting</a>         
+
+</div>
+
+</div>`
+
+const bodyElement = document.querySelector('body');
+bodyElement.insertAdjacentHTML('afterbegin', menuContent);
