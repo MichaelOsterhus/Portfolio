d5568573d433e6a2d951e21174da971c83be7bcf
MichaelOsterhus <michaelosterhus278@gmail.com>
2023-03-31 17:28:29 -0400

    pushing data.items

--- js/blogpost.js
+++ js/blogpost.js
@@ -34,7 +34,7 @@ fetch(`https://www.googleapis.com/blogger/v3/blogs/${blogId2}/posts?key=${apiKey
     throw new Error('Request failed');
   })
   .then(data => {
-    blogs.push(data)
+    blogs.push(data.items)
     console.log(data);
     console.log(blogs);
   })
