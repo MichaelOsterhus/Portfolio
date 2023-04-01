acdf3ef8fb0c22efa45e4a779308a49499ab2822
MichaelOsterhus <michaelosterhus278@gmail.com>
2023-04-01 12:15:32 -0400

    getting image to draw

--- js/background.js
+++ js/background.js
@@ -10,14 +10,14 @@ const img = new Image();
 const scale = img.width / canvas.width
 img.src = '../img/background-sketch.png';
 img.onload = function() {
-    const sourceX = 100;
-  const sourceY = 100;
+    const sourceX = 0;
+  const sourceY = 0;
   const sourceWidth = img.width * 2/3;
   const sourceHeight = img.height * 2/3;
   const destX = 0;
   const destY = 0;
-  const destWidth = sourceWidth * scale;
-  const destHeight = sourceHeight * scale;
+  const destWidth = sourceWidth;
+  const destHeight = sourceHeight;
   ctx.drawImage(img, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
 }
 
