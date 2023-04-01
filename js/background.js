d42d87a563418cfeb970ca8eda56f53a0792de48
MichaelOsterhus <michaelosterhus278@gmail.com>
2023-04-01 11:42:22 -0400

    drawing image to canvas

--- js/background.js
+++ js/background.js
@@ -5,3 +5,9 @@ canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 const xc = canvas.width / 2; // center of canvas
 const yc = canvas.height / 2;
+
+const img = new Image();
+img.src = '../img/background-sketch.png';
+img.onload = function() {
+  ctx.drawImage(img, 0, 0);
+}
