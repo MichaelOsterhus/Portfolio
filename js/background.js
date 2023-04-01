4ac08f4fb70360a12bd6d41b3b8a512a22511113
MichaelOsterhus <michaelosterhus278@gmail.com>
2023-04-01 13:03:14 -0400

    Craziness

--- js/background.js
+++ js/background.js
@@ -14,49 +14,42 @@ img.onload = function() {
   console.log(`The image width is ${img.width}`)
   console.log(`The scale is ${scale}`)
     // Create a temporary canvas element
-    const tempCanvas = document.createElement('canvas');
-    tempCanvas.width = img.width;
-    tempCanvas.height = img.height;
-  
+  const tempCanvas = document.createElement('canvas');
+
     // Draw the image onto the temporary canvas
-    const tempContext = tempCanvas.getContext('2d');
-    tempContext.drawImage(img, 0, 0);
+  const tempContext = tempCanvas.getContext('2d');
   const sourceX = 200
   const sourceY = 300
-  const sourceWidth = img.width * 2/3 - sourceX;
-  const sourceHeight = img.height * 2/3 - sourceY;
-  const scale2 = sourceWidth / canvas.width;
-  const destWidth = canvas.width;
-  const destHeight = sourceHeight;
-  ctx.drawImage(img, sourceX, sourceY, sourceWidth, sourceHeight, 0, 0, destWidth, destHeight);
+  const tWidth = img.width * 2/3
+  const tHeight = img.width * 2/3 
+  tempCanvas.width = tWidth - sourceX;
+  tempCanvas.height = tHeight - sourceY;
+  const destWidth = tempCanvas.width;
+  const destHeight = tempCanvas.height;
+  tempContext.drawImage(img, sourceX, sourceY, tWidth, tHeight, 0, 0, destWidth, destHeight);
+    // Get the pixel data for the canvas
+  const imageData = tempContext.getImageData(0, 0, tempCanvas.width, tempCanvas.height);
+  const pixelData = imageData.data;
+  
+    // Loop through the pixel data and remove white pixels
+  for (let i = 0; i < pixelData.length; i += 4) {
+    const r = pixelData[i];
+    const g = pixelData[i + 1];
+    const b = pixelData[i + 2]; 
+         if (r === 255 && g === 255 && b === 255) {
+        pixelData[i + 3] = 0; // Set alpha to 0 for white pixels
+      }
+    }
+  
+    // Put the modified pixel data back onto the temporary canvas
+    tempContext.putImageData(imageData, 0, 0);
+  
+    // Draw the modified image onto your main canvas
+    context.drawImage(tempCanvas, 0, 0);
 }
 
 
 
-// Wait for the image to load
-img.onload = function() {
-
-
-  // Get the pixel data for the canvas
-  const imageData = tempContext.getImageData(0, 0, tempCanvas.width, tempCanvas.height);
-  const pixelData = imageData.data;
-
-  // Loop through the pixel data and remove white pixels
-  for (let i = 0; i < pixelData.length; i += 4) {
-    const r = pixelData[i];
-    const g = pixelData[i + 1];
-    const b = pixelData[i + 2];
-    if (r === 255 && g === 255 && b === 255) {
-      pixelData[i + 3] = 0; // Set alpha to 0 for white pixels
-    }
-  }
-
-  // Put the modified pixel data back onto the temporary canvas
-  tempContext.putImageData(imageData, 0, 0);
-
-  // Draw the modified image onto your main canvas
-  context.drawImage(tempCanvas, 0, 0);
-};
 
 // Set the source of the image object
 img.src = 'path/to/your/image.png';
