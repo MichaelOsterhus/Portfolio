030a26b393a7cda10b7bf6e0cdc69232a6988a10
MichaelOsterhus <michaelosterhus278@gmail.com>
2023-04-01 11:39:40 -0400

    test page canvas plus blog update

---
+++ js/background.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+const xc = canvas.width / 2; // center of canvas
+const yc = canvas.height / 2;
