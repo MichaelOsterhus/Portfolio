02a1b0d7083fced28b56be8a9bc384ac35363e36
MichaelOsterhus <michaelosterhus278@gmail.com>
2023-04-01 11:55:07 -0400

    adding js folder to url

--- js/background.js
+++ js/background.js
@@ -11,3 +11,18 @@ img.src = '../img/background-sketch.png';
 img.onload = function() {
   ctx.drawImage(img, 0, 0);
 }
+
+
+// const img = new Image();
+// img.src = 'image.png';
+// img.onload = function() {
+//   const sourceX = 0;
+//   const sourceY = 0;
+//   const sourceWidth = img.width * 2/3;
+//   const sourceHeight = img.height * 2/3;
+//   const destX = 0;
+//   const destY = 0;
+//   const destWidth = sourceWidth;
+//   const destHeight = sourceHeight;
+//   ctx.drawImage(img, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
+// }
