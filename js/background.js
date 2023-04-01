0f9dca43804953e323207121de529b6d26900f10
MichaelOsterhus <michaelosterhus278@gmail.com>
2023-04-01 12:34:28 -0400

    moving sourceX and sourceY

--- js/background.js
+++ js/background.js
@@ -13,8 +13,8 @@ img.onload = function() {
   console.log(`The canvas width is ${canvas.width}`)
   console.log(`The image width is ${img.width}`)
   console.log(`The scale is ${scale}`)
-  const sourceX = 0
-  const sourceY = 0
+  const sourceX = 100
+  const sourceY = 100
   const sourceWidth = img.width * 2/3 - sourceX;
   const sourceHeight = img.height * 2/3 - sourceY;
   const scale2 = sourceWidth / canvas.width;
@@ -24,16 +24,3 @@ img.onload = function() {
 }
 
 
-// const img = new Image();
-// img.src = 'image.png';
-// img.onload = function() {
-//   const sourceX = 0;
-//   const sourceY = 0;
-//   const sourceWidth = img.width * 2/3;
-//   const sourceHeight = img.height * 2/3;
-//   const destX = 0;
-//   const destY = 0;
-//   const destWidth = sourceWidth;
-//   const destHeight = sourceHeight;
-//   ctx.drawImage(img, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
-// }
