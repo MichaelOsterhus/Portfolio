487280c7c8b6d93b5ff349aeef7a730b8629ddfc
MichaelOsterhus <michaelosterhus278@gmail.com>
2023-04-01 12:18:14 -0400

    cleaning code

--- js/background.js
+++ js/background.js
@@ -11,15 +11,11 @@ const scale = img.width / canvas.width
 console.log(scale)
 img.src = '../img/background-sketch.png';
 img.onload = function() {
-    const sourceX = 0;
-  const sourceY = 0;
   const sourceWidth = img.width * 2/3;
   const sourceHeight = img.height * 2/3;
-  const destX = 0;
-  const destY = 0;
   const destWidth = sourceWidth;
   const destHeight = sourceHeight;
-  ctx.drawImage(img, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
+  ctx.drawImage(img, 0, 0, sourceWidth, sourceHeight, 0, 0, destWidth, destHeight);
 }
 
 
