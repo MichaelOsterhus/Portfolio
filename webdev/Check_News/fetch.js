658ca9a22afa95bf09e17d3186e1813d00f59d51
MichaelOsterhus <michaelosterhus278@gmail.com>
2023-04-04 18:43:01 -0400

    Adding forEach to news fetch

--- webdev/Check_News/fetch.js
+++
@@ -1,7 +0,0 @@
-const url = `https://api.gdeltproject.org/api/v2/doc/doc?query=artificial+intelligence&mode=artlist&timespan=1M&format=json`;
-
-
-fetch(url)
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
