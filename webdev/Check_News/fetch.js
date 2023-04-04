0cea4c8d5d12328bb4063ae893a46db36bc87947
MichaelOsterhus <michaelosterhus278@gmail.com>
2023-04-04 17:54:58 -0400

    Trying GDELT

--- webdev/Check_News/fetch.js
+++ webdev/Check_News/fetch.js
@@ -1,34 +1,7 @@
-// URL of the RSS feed
-// URL of the RSS feed
-const rssUrl = [
-    'https://www.reutersagency.com/feed/?best-topics=tech&post_type=best',
-    'https://www.reutersagency.com/feed/?best-sectors=economy&post_type=best',
-    'https://www.reutersagency.com/feed/?best-topics=human-interest&post_type=best'
-  ];
-  
-  // Fetch the RSS feeds using the fetch API
-  rssUrl.forEach(url => {
-    fetch(url)
-      .then(response => response.text())
-      .then(xmlText => {
-        // Parse the XML and get the feed items
-        const parser = new DOMParser();
-        const xmlDoc = parser.parseFromString(xmlText, 'text/xml');
-        const items = xmlDoc.querySelectorAll('item');
-  
-        // Display the feed items
-        items.forEach(item => {
-          const title = item.querySelector('title').textContent;
-          const link = item.querySelector('link').textContent;
-          const description = item.querySelector('description').textContent;
-          const newsfeed = document.getElementById('newsfeed');
-          const a = document.createElement('a');
-          a.href = link;
-          a.innerHTML = `<h2>${title}</h2>
-            <p>${description}</p>`;
-          newsfeed.appendChild(a); 
-        });
-      })
-      .catch(error => console.error(error));
-  });
-  
+const url = `https://api.gdeltproject.org/api/v2/doc/doc?query=artificial+intelligence&mode=artlist&timespan=1M&format=json`;
+
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
