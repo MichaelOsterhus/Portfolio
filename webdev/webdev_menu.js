71d3602f948378f13d1e2cdf85ab36cc3b256414
MichaelOsterhus <michaelosterhus278@gmail.com>
2023-04-04 09:36:31 -0400

    Working on webdev

---
+++ webdev/webdev_menu.js
@@ -0,0 +1,15 @@
+//logo
+const menuContent = `<a href="../../index.html">
+<div class="logo">
+   <img src="../../img/4EC_logo32.png"/>
+<h1>Michael Osterhus</h1>
+</div>
+</a>`
+
+const bodyElement = document.querySelector('body');
+bodyElement.insertAdjacentHTML('afterbegin', menuContent);
+
+
+//projects
+const projArray = []
+const project = document.getElementById('projects')
