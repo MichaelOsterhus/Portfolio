00249c337f7e916a310a9ca1ac7b9dc103ffb739
MichaelOsterhus <michaelosterhus278@gmail.com>
2023-04-04 23:07:55 -0400

    reverting back to newsfeed without translation

--- webdev/Check_News/news_fetch.js
+++ webdev/Check_News/news_fetch.js
@@ -1,73 +1,64 @@
-import { setCORS } from "google-translate-api-browser";
-// setting up cors-anywhere server address
-const translate = setCORS("http://cors-anywhere.herokuapp.com/");
 
-// Example usage
-// translate('Bonjour', { to: 'en' }).then(res => {
-//   console.log(res.text);
-// }).catch(err => {
-//   console.error(err);
-// });
 
 
 const url = `https://api.gdeltproject.org/api/v2/doc/doc?query=artificial+intelligence&mode=artlist&timespan=1M&format=json`;
 
-// const newsfeed = document.getElementById('newsfeed')
-// const trans = (story) => {
-//   const link = document.createElement('a')
-//   const title = story.title
-//   link.setAttribute('href', story.url)
-//   link.innerHTML = `<h1>${title}</h1>`
-//   newsfeed.appendChild(link)
-// }
-
-// fetch(url)
-//   .then(response => response.json())
-//   .then(data => {
-//     data.articles.forEach((story) => trans(story))
-//     console.log(data)})
-//   .catch(error => console.error(error));
-
-const newsfeed = document.getElementById('newsfeed');
-
+const newsfeed = document.getElementById('newsfeed')
 const trans = (story) => {
-  // Create a new div element to hold the link and subheading
-  const div = document.createElement('div');
-
-  // Translate the title and add it as a subheading
-  translate(story.title, { to: 'en' }).then(res => {
-    const subheading = document.createElement('h2');
-    subheading.innerText = res.text;
-    div.appendChild(subheading);
-  }).catch(err => {
-    console.error(err);
-  });
-
-  // Create a link element for the original title
-  const link = document.createElement('a');
-  link.setAttribute('href', story.url);
-  link.innerHTML = `<h1>${story.title}</h1>`;
-  div.appendChild(link);
-
-  // Translate the description and add it as a paragraph
-  translate(story.description, { to: 'en' }).then(res => {
-    const paragraph = document.createElement('p');
-    paragraph.innerText = res.text;
-    div.appendChild(paragraph);
-  }).catch(err => {
-    console.error(err);
-  });
-
-  // Add the div to the newsfeed
-  newsfeed.appendChild(div);
+  const link = document.createElement('a')
+  const title = story.title
+  link.setAttribute('href', story.url)
+  link.innerHTML = `<h1>${title}</h1>`
+  newsfeed.appendChild(link)
 }
 
 fetch(url)
   .then(response => response.json())
   .then(data => {
-    data.articles.forEach((story) => trans(story));
-    console.log(data);
-  })
+    data.articles.forEach((story) => trans(story))
+    console.log(data)})
   .catch(error => console.error(error));
+
+// const newsfeed = document.getElementById('newsfeed');
+
+// const trans = (story) => {
+//   // Create a new div element to hold the link and subheading
+//   const div = document.createElement('div');
+
+//   // Translate the title and add it as a subheading
+//   translate(story.title, { to: 'en' }).then(res => {
+//     const subheading = document.createElement('h2');
+//     subheading.innerText = res.text;
+//     div.appendChild(subheading);
+//   }).catch(err => {
+//     console.error(err);
+//   });
+
+//   // Create a link element for the original title
+//   const link = document.createElement('a');
+//   link.setAttribute('href', story.url);
+//   link.innerHTML = `<h1>${story.title}</h1>`;
+//   div.appendChild(link);
+
+//   // Translate the description and add it as a paragraph
+//   translate(story.description, { to: 'en' }).then(res => {
+//     const paragraph = document.createElement('p');
+//     paragraph.innerText = res.text;
+//     div.appendChild(paragraph);
+//   }).catch(err => {
+//     console.error(err);
+//   });
+
+//   // Add the div to the newsfeed
+//   newsfeed.appendChild(div);
+// }
+
+// fetch(url)
+//   .then(response => response.json())
+//   .then(data => {
+//     data.articles.forEach((story) => trans(story));
+//     console.log(data);
+//   })
+//   .catch(error => console.error(error));
 
 
