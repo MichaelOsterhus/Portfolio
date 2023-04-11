2f9f8660b0eb43ec2f83d49934c8a634946f0325
MichaelOsterhus <michaelosterhus278@gmail.com>
2023-04-11 02:44:38 +0000

    going to bed

--- webdev/Dynamic_Map/NCmap.js
+++ webdev/Dynamic_Map/NCmap.js
@@ -1,3 +1,44 @@
+// Wait for the window to load before querying the SVG
+window.addEventListener('load', function() {
+
+  // Get the SVG element by ID
+  var svg = document.getElementById('nc-map');
+
+  // Get all path elements in the SVG
+  var paths = svg.querySelectorAll('path');
+
+  // Add the "county" class to all path elements
+  paths.forEach(function(path) {
+    path.classList.add('county');
+  });
+
+  // Set the fill color of all path elements
+  paths.forEach(function(path) {
+    path.style.fill = '#fff';
+  
+  
+  
+  });
+
+  // Create a list of county names
+  var countyList = document.createElement('ul');
+  paths.forEach(function(path) {
+    var countyName = path.id.replace('_', ' ');
+    var listItem = document.createElement('li');
+    listItem.textContent = countyName;
+    countyList.appendChild(listItem);
+  });
+
+  // Add the list to the page
+  var counties = document.getElementById('counties');
+  counties.appendChild(countyList);
+
+});
+
+
+
+
+
 var console = document.createElement("div");
 console.setAttribute("id", "console");
 document.body.appendChild(console);
@@ -13,42 +54,38 @@ console.log = function(message) {
 
 // test the console
 
-console.log('hi there')
-
+// function newFunction() {
+//   var svg = document.getElementById("nc-map");
+//   var paths = svg.querySelectorAll("path");
+//   console.log(`He there paths are where `);
+// }
 // Get all path elements in the SVG
 // Get all path elements in the SVG
 // var paths = document.querySelectorAll("path");
-var svg = document.getElementById("nc-map");
-var paths = svg.querySelectorAll("path");
 
-console.log(paths) 
 // Add the "county" class to all path elements
 
 // Change the fill color of all path elements
+// console.log('hi there')
+
+// console.log(test) 
+
+// // Create an unordered list for the county names
+// var counties = document.getElementById('counties');
+// var header = document.createElement('h1')
+// header.innerHTML = `The Fist Count is ${paths[0].id}`
+
+// console.log(test) 
+
+// counties.appendChild(header)
+// var list = document.createElement('ul');
+// counties.appendChild(list);
+ 
+// console.log(test) 
+// var testDiv = document.createElement('h1')
+// testDiv.textContent = test
+// counties.appendChild(testDiv)
 
 
-console.log(test) 
 
-// Create an unordered list for the county names
-var counties = document.getElementById('counties');
-var header = document.createElement('h1')
-header.innerHTML = `The Fist Count is ${paths[0].id}`
-counties.appendChild(header)
-var list = document.createElement('ul');
-counties.appendChild(list);
-
-// Append each county name to the list
-var pathArray = Array.from(paths);
-pathArray.forEach(function(path) {
-  var listItem = document.createElement('li');
-  listItem.innerHTML = path.id;
-  list.appendChild(listItem);
-});
-
-// Test sentence
-var test = "The first county is " + pathArray[0].id;
-var testDiv = document.createElement('h1')
-testDiv.textContent = test
-counties.appendChild(testDiv)
-
-
+// console.log(test) 
