ba19c0f7390b8542eead976d10381647df8bd28d
MichaelOsterhus <michaelosterhus278@gmail.com>
2023-04-13 10:49:14 -0400

    Working on Map

--- webdev/Dynamic_Map/NCmap.js
+++ webdev/Dynamic_Map/NCmap.js
@@ -1,25 +1,44 @@
+
 window.addEventListener('load', function() {
 
   // Get the SVG element by ID
-var svgObject = document.getElementById('svg-object').contentDocument;
-// var svg = svgObject.getElementById("NC_Hyde");
+var svg = document.getElementById('svg-object').contentDocument;
+// var svg = svg.getElementById("NC_Hyde");
 // console.log(svg);
 
   // Get all path elements in the SVG
-var paths = svgObject.querySelectorAll('path');
+var paths = svg.querySelectorAll('path');
 let pathArray = []
 paths.forEach(function(path) {
    pathArray.push(path);
 });
 //??dkfwo
+
+var svgObject = document.getElementById('svg-object')
+const viewportWidth = window.innerWidth; // get the viewport width of the browser window
+const displayedMapWidth = svgObject.getBoundingClientRect().width; // get the displayed map width
+const displayedMapHeight = svgObject.getBoundingClientRect().height; // get the displayed map height
+const svgTag = svg.querySelector('svg')
+const viewBox = svgTag.getAttribute('viewBox'); // get the viewBox attribute value
+const viewBoxValues = viewBox.split(' ').map(parseFloat); // parse and convert the values to numbers
+const viewBoxWidth = viewBoxValues[2]; // get the width value
+const viewBoxHeight = viewBoxValues[3]; // get the width value
+const scaleFactor = displayedMapWidth / viewBoxWidth
+
+console.log(`The viewport width = ${viewportWidth}`)
+console.log(`The display width = ${displayedMapWidth} the height = ${displayedMapHeight} and the ratio is ${displayedMapWidth / displayedMapHeight}`)
+console.log(`The viewBox width = ${viewBoxWidth} the height = ${viewBoxHeight} and the ratio is ${viewBoxWidth / viewBoxHeight}`)
+console.log(`The svg-object width = ${svgObject.width}`)
+
+
 function calculatePathCenter(pathString) {
   let points = pathString.match(/(-?\d+(\.\d+)?)/g).map(Number); // extract numbers from path string
   let xMin = Math.min(...points.filter((p, i) => i % 2 === 0)); // find minimum x value
   let xMax = Math.max(...points.filter((p, i) => i % 2 === 0)); // find maximum x value
   let yMin = Math.min(...points.filter((p, i) => i % 2 === 1)); // find minimum y value
   let yMax = Math.max(...points.filter((p, i) => i % 2 === 1)); // find maximum y value
-  let centerX = (xMin + xMax) / 2; // calculate x coordinate of center
-  let centerY = (yMin + yMax) / 2; // calculate y coordinate of center
+  let centerX = (xMin + xMax) / 2 * scaleFactor; // calculate x coordinate of center
+  let centerY = (yMin + yMax) / 2 * scaleFactor; // calculate y coordinate of center
   return { x: centerX, y: centerY }; // return center point as object
 }
 
@@ -28,28 +47,34 @@ paths.forEach(function(path) {
     path.style.stroke = '#666'
     path.style.strokeWidth = .4
     const d = path.getAttribute('d')
-
   });
 
 var counties = document.getElementById('counties');
+const countyNames = [];
+
 for (i = 0; i < paths.length; i++) {
   var countyName = paths[i].id.replace('NC_', ' '); 
-  var div = document.createElement('div');
-  div.id = paths[i].id
-  div.classList.add('county-name')
-  div.textContent = countyName;
-  const newD = paths[i].getAttribute('d')
-  div.style.top = `${calculatePathCenter(newD).y}px`
-  div.style.left = `${calculatePathCenter(newD).x}px`
-  counties.appendChild(div);
-};
+  countyNames.push(countyName);
+}
+countyNames.sort()
 
-const countyNames = document.querySelectorAll('.county-name');
+for (i = 0; i < countyNames.length; i++) {
+  var div = document.createElement('div');
+  div.id = countyNames[i]
+  div.classList.add('county-name')
+  div.textContent = countyNames[i]
+  counties.appendChild(div);
+  console.log(`The name of the county is: ${countyNames[i]}`)
+}
+
+
+
+
 // console.log(countyNames)
-document.querySelector('#svg-object').addEventListener('load', function () {
+document.querySelectorAll('county-name').addEventListener('load', function () {
 
   paths.forEach(path => {
-    const countyName = svgObject.getElementById(path.id);
+    const countyName = svg.getElementById(path.id);
   
     path.addEventListener('mouseover', function () {
       countyNames.forEach(name => {
@@ -64,6 +89,11 @@ document.querySelector('#svg-object').addEventListener('load', function () {
     });
   });
 });
+
+
+
+
+
 });
 
 // function calculatePathCenter(pathString) {
