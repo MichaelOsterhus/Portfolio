08c3b6863594a65a8e04c2a22410cb378d3fcd8b
MichaelOsterhus <michaelosterhus278@gmail.com>
2023-04-14 12:50:19 -0400

    Completed county highlight on mouse hover

--- webdev/Dynamic_Map/NCmap.js
+++ webdev/Dynamic_Map/NCmap.js
@@ -9,37 +9,23 @@ let pathArray = []
 paths.forEach(function(path) {
    pathArray.push(path);
 });
-pathArray.sort()
-
-var svgObject = document.getElementById('svg-object')
-const viewportWidth = window.innerWidth; // get the viewport width of the browser window
-const displayedMapWidth = svgObject.getBoundingClientRect().width; // get the displayed map width
-const displayedMapHeight = svgObject.getBoundingClientRect().height; // get the displayed map height
-const svgTag = svg.querySelector('svg')
-const viewBox = svgTag.getAttribute('viewBox'); // get the viewBox attribute value
-const viewBoxValues = viewBox.split(' ').map(parseFloat); // parse and convert the values to numbers
-const viewBoxWidth = viewBoxValues[2]; // get the width value
-const viewBoxHeight = viewBoxValues[3]; // get the width value
-const scaleFactor = displayedMapWidth / viewBoxWidth
-
-// console.log(`The viewport width = ${viewportWidth}`)
-// console.log(`The display width = ${displayedMapWidth} the height = ${displayedMapHeight} and the ratio is ${displayedMapWidth / displayedMapHeight}`)
-// console.log(`The viewBox width = ${viewBoxWidth} the height = ${viewBoxHeight} and the ratio is ${viewBoxWidth / viewBoxHeight}`)
-// console.log(`The svg-object width = ${svgObject.width}`)
+let alphaPathArray = pathArray.sort((a, b) => {
+  let idA = a.getAttribute('id');
+  let idB = b.getAttribute('id');
+  if (idA < idB) {
+    return -1;
+  }
+  if (idA > idB) {
+    return 1;
+  }
+  return 0;
+});
 
 
-function calculatePathCenter(pathString) {
-  let points = pathString.match(/(-?\d+(\.\d+)?)/g).map(Number); // extract numbers from path string
-  let xMin = Math.min(...points.filter((p, i) => i % 2 === 0)); // find minimum x value
-  let xMax = Math.max(...points.filter((p, i) => i % 2 === 0)); // find maximum x value
-  let yMin = Math.min(...points.filter((p, i) => i % 2 === 1)); // find minimum y value
-  let yMax = Math.max(...points.filter((p, i) => i % 2 === 1)); // find maximum y value
-  let centerX = (xMin + xMax) / 2 * scaleFactor; // calculate x coordinate of center
-  let centerY = (yMin + yMax) / 2 * scaleFactor; // calculate y coordinate of center
-  return { x: centerX, y: centerY }; // return center point as object
-}
+
 
 paths.forEach(function(path) {
+    path.style.transition = "fill 0.8s ease-out";
     path.style.fill = '#000';
     path.style.stroke = '#666'
     path.style.strokeWidth = .4
@@ -65,40 +51,50 @@ for (i = 0; i < countyNames.length; i++) {
 }
 
 const cName = document.querySelectorAll('.county-name')
+
 cName.forEach(function(name) {
   const cIndex = countyNames.indexOf(name.id)
   name.addEventListener('mouseover', function () {
-    pathArray[cIndex].style.fill = 'red'
+    alphaPathArray[cIndex].style.fill = 'red'
   })
   name.addEventListener('mouseout', function () {
-    pathArray[cIndex].style.fill = 'black'
+    alphaPathArray[cIndex].style.fill = 'black'
   });
 })
 
 
-// console.log(countyNames)
-// document.querySelectorAll('county-name').addEventListener('load', function () {
-
-//   paths.forEach(path => {
-//     const countyName = svg.getElementById(path.id);
-  
-//     path.addEventListener('mouseover', function () {
-//       countyNames.forEach(name => {
-//         name.style.display = 'none';
-//       });
-
-//       countyName.style.display = 'block';
-//     });
-
-//  
-//   });
-// });
-
-
-
-
-
 });
+
+//Code I didn't use
+
+// var svgObject = document.getElementById('svg-object')
+
+// const viewportWidth = window.innerWidth; // get the viewport width of the browser window
+// const displayedMapWidth = svgObject.getBoundingClientRect().width; // get the displayed map width
+// const displayedMapHeight = svgObject.getBoundingClientRect().height; // get the displayed map height
+// const svgTag = svg.querySelector('svg')
+// const viewBox = svgTag.getAttribute('viewBox'); // get the viewBox attribute value
+// const viewBoxValues = viewBox.split(' ').map(parseFloat); // parse and convert the values to numbers
+// const viewBoxWidth = viewBoxValues[2]; // get the width value
+// const viewBoxHeight = viewBoxValues[3]; // get the width value
+// const scaleFactor = displayedMapWidth / viewBoxWidth
+
+// console.log(`The viewport width = ${viewportWidth}`)
+// console.log(`The display width = ${displayedMapWidth} the height = ${displayedMapHeight} and the ratio is ${displayedMapWidth / displayedMapHeight}`)
+// console.log(`The viewBox width = ${viewBoxWidth} the height = ${viewBoxHeight} and the ratio is ${viewBoxWidth / viewBoxHeight}`)
+// console.log(`The svg-object width = ${svgObject.width}`)
+
+
+// function calculatePathCenter(pathString) {
+//   let points = pathString.match(/(-?\d+(\.\d+)?)/g).map(Number); // extract numbers from path string
+//   let xMin = Math.min(...points.filter((p, i) => i % 2 === 0)); // find minimum x value
+//   let xMax = Math.max(...points.filter((p, i) => i % 2 === 0)); // find maximum x value
+//   let yMin = Math.min(...points.filter((p, i) => i % 2 === 1)); // find minimum y value
+//   let yMax = Math.max(...points.filter((p, i) => i % 2 === 1)); // find maximum y value
+//   let centerX = (xMin + xMax) / 2 * scaleFactor; // calculate x coordinate of center
+//   let centerY = (yMin + yMax) / 2 * scaleFactor; // calculate y coordinate of center
+//   return { x: centerX, y: centerY }; // return center point as object
+// }
 
 // function calculatePathCenter(pathString) {
 //   let points = pathString.match(/(-?\d+(\.\d+)?)/g).map(Number); // extract numbers from path string
@@ -129,6 +125,12 @@ cName.forEach(function(name) {
 //   return { x: centerX, y: centerY }; // return center point as object
 // }
 
+// print the sorted array
+// for (i = 0; i < alphaPathArray.length; i++) {
+//   console.log(`This is the alphaArray ${alphaPathArray[i].id}`);
+// }
+// console.log(`This is the alphaArray ${alphaPathArray[0].id}`);
+// console.log(`This is the pathArray ${pathArray[0].id}`);
 
 
 
