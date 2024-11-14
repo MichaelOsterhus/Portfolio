46752c435e00c974d0d67150176fc636740e8728
MichaelOsterhus <michaelosterhus278@gmail.com>
2024-11-14 12:32:45 -0500

    blogs and store

--- js/menu.js
+++ js/menu.js
@@ -1,24 +1,4 @@
-const menuContent = `<a href="index.html">
-<div class="logo">
-   <img src="img/4EC_logo32.png"/>
-<h1>Michael Osterhus</h1>
-</div>
-</a>
-
-<div id="wrapper"> 
-<div class="wrap">
-
-   <h3>PROJECTS</h3>
-   <a class="goal" href="webdev/webdev.html">Coding Projects</a>
-   <a class="goal" href="blogs//webdev.html">Coding Projects</a>
-   <a class="goal" href="blogs.html">Writing Projects</a>
-   <a class="goal" href="vidanim.html">Video / Animation</a>
-   <a class="goal" href="digpaint.html">Digital Painting</a>   
-   
-
-</div>
-
-</div>`
+const menuContent = ``
 
 const bodyElement = document.querySelector('body');
 bodyElement.insertAdjacentHTML('afterbegin', menuContent);
